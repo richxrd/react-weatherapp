a5df680c3e4815b966f245722a807f55141aecbe
richxrd <52021889+richxrd@users.noreply.github.com>
2022-05-06 14:54:54 -0400

    Update forwardData.js

--- src/api/forwardData.js
+++ src/api/forwardData.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+API_KEY = "efd037a22f14d6888e3f07f23e4cdba2"
+
 export default axios.create({
     baseURL: "https://api.openweathermap.org/data/2.5",
     params: {
         exclude: "minutely,alerts",
         units: "imperial",
-        appid: process.env.API_KEY,
+        appid: API_KEY,
     },
 });
